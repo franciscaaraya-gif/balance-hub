880bb23f11d067f484ec413e6d293aa52d193f57
Francisca Araya <Franciscap.araya@gmail.com>
2026-04-23 00:49:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-469682222-689b2",
+  "appId": "1:590161106310:web:b9d094e5690f3085594807",
+  "apiKey": "AIzaSyD_qr2yNkeoxTDw6ngEEkSp361WaOOTilo",
+  "authDomain": "studio-469682222-689b2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "590161106310"
+};
